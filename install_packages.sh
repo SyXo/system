949b8b4f52f58bb7be16ab6d907fb609c99e6321
joakimaling <joakim.aling@gmail.com>
2017-11-07 11:22:41 +0100

    Updated the list of installed packages

--- install_packages.sh
+++ install_packages.sh
@@ -9,9 +9,10 @@ sudo pacman -U packer-*.pkg.tar.xz
 
 # install packages
 packer -S --needed --noconfirm --noedit \
-allegro conky cups discord dosbox dropbox feh git gutenprint htop ibus openssh \
-mplayer neofetch ranger rxvt-unicode steam texlive-core tmux transmission-cli \
-ttf-hack vim virtualbox wine-staging
+allegro awesome conky cups discord dosbox dropbox feh git gutenprint htop ibus \
+openssh mplayer neofetch ranger rxvt-unicode steam texlive-core tmux \
+transmission-cli ttf-hack vim virtualbox wine-staging xcompmgr xrog-server \
+xorg-xinit
 
 # fetch dotfiles
 mkdir ~/Code && cd ~/Code
