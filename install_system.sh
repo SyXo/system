8289af21c93c65440d86ea687e201e4f01fd6a37
joakimaling <joakim.aling@gmail.com>
2017-11-07 16:25:47 +0100

    Updated the system script

--- install_system.sh
+++ install_system.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
 #
-# This is a script whihc installs my hand-crafted environment
+# This is a script which installs my hand-crafted environment
 #
+
+# custom settings
+_HOSTNAME=neko
+_KEYMAP=sv-latin1
+_LANG=en_GB.UTF-8
+_TIMEZONE=Europe/Stockholm
+_USER=tora_chan
 
 dialogue() {
   while true; do
@@ -31,19 +38,9 @@ printf "Welcome! This script will install a system with %s on %s!\n" "$(_awesome
 printf "\e[0;31mBeware that you'll lose any not previously backed up data!\e[0m\n\n"
 dialogue "Do you wish to proceed?" || { printf "\nBye!\n"; exit; }
 
-read -p "Enter a username: " name
-
-
- 
-
-
-
-
-
- #lsblk
+lsblk
 
 # create partitions
-
 
 
 # create file systems
@@ -58,21 +55,40 @@ read -p "Enter a username: " name
 #mount /dev/sda1 /mnt/home
 
 # install system
-#pacstrap -i /mnt base base-devel
+pacstrap -i /mnt base base-devel grub
 
-#
-#genfstab -U -p /mnt >> /mnt/etc/fstab
+# write partition table
+genfstab -U -p /mnt >> /mnt/etc/fstab
 
-# 
-#arch-chroot /mnt /bin/bash
+# enter system
+arch-chroot /mnt /bin/bash
 
-#pacman -S grub
-#grub-mkconfig -o /boot/grub/grub.cfg
-#grub-install --recheck /dev/sda
+# install boot loader
+grub-install --recheck /dev/sda
+grub-mkconfig -o /boot/grub/grub.cfg
+
+# disable pc speaker
+echo "blacklist pcspkr" > /etc/modprobe.d/nobeep.conf
+rmmod pcspkr
+
+# set language & timezone
+sed -i 's/#$_LANG/$_LANG/g' /etc/locale.gen
+locale-gen
+localectl set-locale LANG=$_LANG
+localectl set-keymap $_KEYMAP
+timedatectl set-timezone $_TIMEZONE
+hostnamectl set-hostname $HOST
+
+# create user
+useradd -m -g users -G lp,vagrant,wheel -s /bin/bash $_USER
+passwd $_USER
+
+# allow sudo usage for wheel
+sed -i 's/ #%wheel ALL=(ALL) ALL/ %wheel ALL=(ALL) ALL/g' /etc/sudoers
 
 # install packages
 curl -O https://raw.githubusercontent.com/joakimaling/system/master/install_packages.sh && ./install_packages.sh
 
-#exit
-#umount -R /mnt
-#reboot
+exit
+umount -R /mnt
+reboot
